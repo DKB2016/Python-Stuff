9ac9e31ea6afa94e1a7025b8a73ec499f83d7c69
djclark_ <david.j.clark95@gmail.com>
2022-08-05 14:58:41 -0500

    squares list

--- IteratingOverString.py
+++ IteratingOverString.py
@@ -9,11 +9,19 @@
 print("This will check to see if there are any upper case letters and display them.")
 text = input("Please enter a quote: ")
 
+chars=[]
 for char in text:
     if char.isupper():
-        print(char)
-
+        chars.append(char)
+print(chars)
 
 for char in text:
     if char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
         print(char)
+
+# Creates an empty list and adds all the squares from 2-100 squared to the list.
+squares=[]
+for num in range(2,100,2):
+    squares.append(num**2)
+
+print(squares)
