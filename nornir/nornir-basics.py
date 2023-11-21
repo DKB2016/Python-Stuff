a18eacc8e2d7d509862aedea55367ebb65f7459f
djclark_ <david.j.clark95@gmail.com>
2023-11-21 12:48:55 -0600

    nornir basics

---
+++ nornir/nornir-basics.py
@@ -0,0 +1,20 @@
+from nornir import InitNornir
+from nornir_netmiko.tasks import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
+
+def main():
+    nr = InitNornir(config_file="c:\\Users\\david\\OneDrive\\Documents\\GitHub\\Python-Stuff\\nornir\\config.yml")
+
+    # Define the task to send a command
+    def get_config(task):
+        result = task.run(netmiko_send_command, command_string="show ip route")
+        print(f"--- {task.host} ---\n{result.result}")
+
+    # Run the task on the device
+    result = nr.run(task=get_config)
+
+    # Print the result
+    print_result(result)
+
+if __name__ == "__main__":
+    main()
