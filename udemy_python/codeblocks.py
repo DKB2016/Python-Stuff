7da673dac3f8521a008873f8052b8a01da534a32
djclark_ <david.j.clark95@gmail.com>
2023-12-06 07:19:51 -0600

    code blocks

---
+++ udemy_python/codeblocks.py
@@ -0,0 +1,11 @@
+#
+#
+#
+
+for i in range(1,13):
+    # when you use a colon(:) you automatically get a new code block(indentation = 4 spaces)
+    print("No. {} squared is {} and cubed is {:4}".format(i, i ** 2, i ** 3))
+    # you can also use tab to create a new code block
+    # code at the same indentation are a part of the same code block
+    print("*" * 80)
+    
